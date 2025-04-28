791d266779a2a78db7adf70f522f22662b78d14a
$@#il <sahilsadekar249775@gmail.com>
2025-04-28 23:12:59 +0530

    started with fronteend

--- apps/frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  clerkPublishableKey: 'your_clerk_publishable_key',
-  apiUrl: '/api'
-};
--- apps/frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  clerkPublishableKey: 'your_clerk_publishable_key',
-  apiUrl: 'http://localhost:3001/api'
-};
